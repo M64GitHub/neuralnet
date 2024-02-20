78ad6a5a856d6cffbacba4187c38dcc0cf010f11
M64GitHub <m64.overdrive@gmail.com>
2024-02-20 03:37:09 +0100

    implemented forwardPropagation()

--- neuralnet.c
+++ neuralnet.c
@@ -52,7 +52,9 @@ typedef struct S_NeuralNetwork {
 // -- helpers
 
 // Sigmoid activation function
-double sigmoid(double x) { return 1.0 / (1.0 + exp(-x)); }
+double sigmoid(double x) { 
+  return 1.0 / (1.0 + exp(-x)); 
+}
 
 // -- nn functions
 
@@ -178,8 +180,90 @@ void setInputValues(NeuralNetwork *network, double *inputValues) {
   }
 }
 
+// function
+double weightedSum(Neuron *n) {
+  double o = 0.0;
+
+  for (int i = 0; i < n->num_inputs; i++) {
+    o += n->input_vals[i] * n->weights[i];
+  }
+
+  return o;
+}
+
 // Function for forward propagation to calculate the output of the network
-void forwardPropagation(NeuralNetwork *network) {}
+void forwardPropagation(NeuralNetwork *network) {
+  // [1] We assume input values have been set: in the input_vals of the
+  // neurons in the i_layer neurons (ni->input_vals[1]).
+  //
+  // Next we "evaluate" the input layer: for each input neuron:
+  // - we calculate the weighted sum (over all indizes incl. 0, the bias).
+  // - we call the activation function on the weighted sum
+  // - we store the result in the neuron's output.
+  // - then we transfer the output to the inputs of the first hidden layer
+  // (h_layers[0]):
+  //   it is important to there store the input values from index 1 onwards,
+  //   as input val[0] is always used for the bias in every neuron:
+  //   - for each neuron we find in the hidden layer[0] we set our output
+  //     as the hidden neuron's input [i+1].
+  //     doing it right here helps to treat all neurons in all hidden layers
+  //     the same in the next big step:
+  //
+  // [2]: Now we "evaluate" all the hidden layers: our neuron index is h.
+  // - for all hidden layers L:
+  //   - for all neurons in the hidden layer: we can do "[1]",
+  //     with the only "difference": we store the neuron's output as
+  //     input_vals[h+1] in all the next hidden layers neurons:
+  //     h_layers[L+1][h].input_vals[h+1] .
+  //     of course we can not do that in the last hidden layer.
+  //     in the last hidden layer we store our output in all output layer's
+  //     neuron's input_vals[h+1] instead.
+  //
+  // [3]: Now we can evaluate the output layer: just calculate the weighted
+  // sum and call the activation function. this is the output of each output
+  // neuron.
+
+  double ws = 0.0;
+  double o = 0.0;
+  // for all input neurons
+  for (int i = 0; i < network->num_inputs; i++) {
+    ws = weightedSum(&network->i_layer[i]);
+    o = sigmoid(ws);
+    network->i_layer[i].output = o;
+    for (int h = 0; h < network->neurons_per_h_layer; h++) {
+      network->h_layers[0][h].input_vals[i + 1] = o;
+    }
+  }
+
+  // hidden layers
+  for (int L = 0; L < network->num_h_layers; L++) {
+    for (int h = 0; h < network->neurons_per_h_layer; h++) {
+      ws = weightedSum(&network->h_layers[L][h]);
+      o = sigmoid(ws);
+      network->h_layers[L][h].output = o;
+
+      // store in next layer or output layer
+      if(L < (network->num_h_layers - 1)) {
+        // store in next layer
+        for (int l = 0; l < network->neurons_per_h_layer; l++) {
+          network->h_layers[L + 1][l].input_vals[h + 1] = o;
+        }
+      } else {
+        // store in output layer
+        for (int l = 0; l < network->num_outputs; l++) {
+          network->o_layer[l].input_vals[h + 1] = o;
+        }
+      } // store in hidden or output layer
+    } // h
+  }   // L
+
+  // output layer: for all output neurons
+  for (int n = 0; n < network->num_outputs; n++) {
+    ws = weightedSum(&network->o_layer[n]);
+    o = sigmoid(ws);
+    network->o_layer[n].output = o;
+  }
+}
 
 void dumpNeuron(Neuron *neuron) {
   Neuron n = *neuron;
@@ -252,11 +336,13 @@ void dumpNetwork(NeuralNetwork *network) {
 
 int main() {
   // 2 inputs, 1 output, 1 hidden layer, layer size: 3
-  NeuralNetwork *network = initializeNetwork(2, 1, 1, 3);
+  NeuralNetwork *network = initializeNetwork(2, 1, 2, 3);
 
   double i_vals[] = {0.1, 0.2};
   setInputValues(network, i_vals);
+  dumpNetwork(network);
 
+  forwardPropagation(network);
   dumpNetwork(network);
 
   freeNetwork(network);
