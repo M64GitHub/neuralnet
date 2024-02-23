7ddae9b16e5d8827899bb521f88dc295c01c27d1
M64GitHub <m64.overdrive@gmail.com>
2024-02-23 01:53:35 +0100

    add age, comments

--- evolution_ga.h
+++ evolution_ga.h
@@ -6,14 +6,18 @@
 // Hyper parameters, specifying the environment
 typedef struct S_World {
   int population_size;
-  double mutation_rate;
-  double selection_pressure;
-  double crossover_rate;
+  double mutation_rate_individuals; // how many individuals will mutate
+  double mutation_rate;             // how many weights will mutate
+  double mutation_strength;         // how much a weight can mutate
+  double selection_pressure; // how many parents need to be chosen from the best
+                             // (fittest) individuals
+  double crossover_rate;     // how many individuals will be selected as parents
 } World;
 
 typedef struct S_Individual {
   NeuralNetwork *network;
   double fitness;
+  int age;
 } Individual;
 
 typedef struct S_Population {
@@ -23,6 +27,7 @@ typedef struct S_Population {
   double current_best_fitness_val;
   Individual **current_best_individuals; // array of pointers
   int current_best_individuals_size;
+  int age;
 } Population;
 
 #endif
