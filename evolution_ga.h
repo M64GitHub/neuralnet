c0386789277c3c1ce41369b92da36832e64cb409
M64GitHub <m64.overdrive@gmail.com>
2024-02-23 01:41:52 +0100

    prepare structs for evolution

--- evolution_ga.h
+++ evolution_ga.h
@@ -3,6 +3,26 @@
 
 #include "neuralnet.h"
 
+// Hyper parameters, specifying the environment
+typedef struct S_World {
+  int population_size;
+  double mutation_rate;
+  double selection_pressure;
+  double crossover_rate;
+} World;
 
+typedef struct S_Individual {
+  NeuralNetwork *network;
+  double fitness;
+} Individual;
+
+typedef struct S_Population {
+  int size;
+  Individual *individuals;
+
+  double current_best_fitness_val;
+  Individual **current_best_individuals; // array of pointers
+  int current_best_individuals_size;
+} Population;
 
 #endif
