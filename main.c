f275a3246fcf6874b07956a7e4c3a49df17c8d9f
M64GitHub <m64.overdrive@gmail.com>
2024-02-28 03:25:02 +0100

    idx fix on weightedsum

--- main.c
+++ main.c
@@ -6,14 +6,13 @@ int main() {
   unsigned long ts1 = 0;
   unsigned long ts2 = 0;
 
-  // 2 inputs, 1 output, 1 hidden layer, layer size: 3, relU
-  NeuralNetwork *network = NN_Network_initialize(2, 1, 4, 10, NN_AF_RELU);
+  // 2 inputs, 1 output, 4 hidden layers, layer size: 10, relU
+  // NeuralNetwork *network = NN_Network_initialize(2, 1, 4, 10, NN_AF_RELU);
   // NN_Network_input_values_set(network, (double[]){2.0, 3.0});
 
   ts1 = get_timestamp();
   // potential values for MNIST
-  // NeuralNetwork *network = NN_Network_initialize(784, 10, 1, 128,
-  // NN_AF_RELU);
+  NeuralNetwork *network = NN_Network_initialize(784, 10, 1, 128, NN_AF_RELU);
   ts2 = get_duration_since(ts1);
   printf(" * initialization took: %lu usecs\n", ts2);
 
@@ -21,7 +20,7 @@ int main() {
   NN_Network_propagate_forward(network);
   ts2 = get_duration_since(ts1);
   printf(" * forward propagation took: %lu usecs\n", ts2);
-  NN_Network_dump(network);
+  // NN_Network_dump(network);
 
   NN_Network_free(network);
 
